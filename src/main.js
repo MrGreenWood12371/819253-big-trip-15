5e511c22599f9a07fd762b672bb118085e745c1e
Никита <kondor12371@yandex.ru>
2021-07-28 16:39:28 +0300

    Исправил ошибки в названиях функций и файлов

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { createSiteMenuTemplate } from './view/site-menu';
-import { createSiteFilterTemplate } from './view/site-fiter';
-import { createTripSortTemplate } from './view/trip-sort';
+import { createSiteNavigationTemplate } from './view/site-navigation';
+import { createTripFilteringFormTemplate } from './view/fitering-form';
+import { createTripSotringFormTemplate } from './view/trip-sorting-form';
 import { createTripRoadInfoTemplate } from './view/trip-road-info';
 import { createTripPriceTemplate } from './view/trip-price';
 import { createTripPointTemplate } from './view/trip-point';
-import { createPointAddTemplate } from './view/point-add-form';
-import { createEditPointTemplate } from './view/point-edit-form';
+import { createAddingEventFormTemplate } from './view/event-form';
+import { createEditingEventFormTemplate } from './view/editing-event-form';
 
 const POINTS_COUNT = 3;
 
@@ -13,20 +13,20 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const siteHeader = document.querySelector('.trip-main');
-const siteHeaderConstrols = siteHeader.querySelector('.trip-controls');
+const siteHeaderElement = document.querySelector('.trip-main');
+const siteHeaderConstrols = siteHeaderElement.querySelector('.trip-controls');
 
 const siteMenu = siteHeaderConstrols.querySelector('.trip-controls__navigation');
-render(siteMenu, createSiteMenuTemplate(), 'beforeend');
+render(siteMenu, createSiteNavigationTemplate(), 'beforeend');
 
 const siteFilter = siteHeaderConstrols.querySelector('.trip-controls__filters');
-render(siteFilter, createSiteFilterTemplate(), 'beforeend');
+render(siteFilter, createTripFilteringFormTemplate(), 'beforeend');
 
 const mainContent = document.querySelector('.trip-events');
-render(mainContent, createTripSortTemplate(), 'beforeend');
+render(mainContent, createTripSotringFormTemplate(), 'beforeend');
 
-render(siteHeader, createTripRoadInfoTemplate(), 'afterbegin');
-const tripInfo = siteHeader.querySelector('.trip-info');
+render(siteHeaderElement, createTripRoadInfoTemplate(), 'afterbegin');
+const tripInfo = siteHeaderElement.querySelector('.trip-info');
 render(tripInfo, createTripPriceTemplate(), 'beforeend');
 
 const tripList = mainContent.querySelector('.trip-events__list');
@@ -34,5 +34,5 @@ for (let i = 0; i < POINTS_COUNT; i++) {
   render(tripList, createTripPointTemplate(), 'beforeend');
 }
 
-render(tripList, createPointAddTemplate(), 'afterbegin');
-render(tripList, createEditPointTemplate(), 'afterbegin');
+render(tripList, createAddingEventFormTemplate(), 'afterbegin');
+render(tripList, createEditingEventFormTemplate(), 'afterbegin');
